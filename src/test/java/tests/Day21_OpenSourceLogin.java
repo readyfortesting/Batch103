863392972202b02c3af1bbbd40091dcf30d1a55d
Tall Boy <tallboy6077@gmail.com>
2023-01-28 21:21:48 +0300

    OpenSourceLogin

---
+++ src/test/java/tests/Day21_OpenSourceLogin.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.OpenSourceDashboardPage;
+import pages.OpenSourcePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class Day21_OpenSourceLogin {
+    @Test
+    public void openSourceLogin() throws InterruptedException {
+//        1. Sayfaya git
+//        Driver.getDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Driver.getDriver().get(ConfigReader.getProperty("open_source_url"));
+
+        //        2. Page Objesi Olustur
+        OpenSourcePage openSourcePage = new OpenSourcePage();
+
+        //       3. Bu objeyi kullanarak o classdaki objelere ulas
+        openSourcePage.username.sendKeys(ConfigReader.getProperty("open_source_username"));
+        openSourcePage.password.sendKeys(ConfigReader.getProperty("open_source_password"));
+        openSourcePage.submitButton.click();
+
+        //        4. Assertion
+        OpenSourceDashboardPage openSourceDashboardPage = new OpenSourceDashboardPage();
+        Thread.sleep(10);
+        Assert.assertTrue(openSourceDashboardPage.dashboardHeader.isDisplayed());
+
+        //    5. Close driver
+        Driver.closeDriver();
+}
+}
