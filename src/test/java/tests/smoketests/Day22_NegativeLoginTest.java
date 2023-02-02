2a73d4de3522633b80f23cdc90a66c69c0fc7084
Tall Boy <tallboy6077@gmail.com>
2023-02-02 22:27:03 +0300

    NegativeLoginTest

---
+++ src/test/java/tests/smoketests/Day22_NegativeLoginTest.java
@@ -0,0 +1,26 @@
+package tests.smoketests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BlueRentalHomePage;
+import pages.BlueRentalLoginPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class Day22_NegativeLoginTest {
+    BlueRentalHomePage blueRentalHomePage;
+    BlueRentalLoginPage blueRentalLoginPage;
+    @Test
+    public void US100208_Negative_Login() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("app_url"));
+        blueRentalHomePage = new BlueRentalHomePage();
+        blueRentalLoginPage = new BlueRentalLoginPage();
+        blueRentalHomePage.loginLink.click();
+        blueRentalLoginPage.emailBox.sendKeys(ConfigReader.getProperty("fake_email"));
+        blueRentalLoginPage.passwordBox.sendKeys(ConfigReader.getProperty("fake_pass"));
+        blueRentalLoginPage.loginButton.click();
+        Thread.sleep(2000);
+        Assert.assertEquals(blueRentalLoginPage.error_message_1.getText(),"User with email fake@bluerentalcars.com not found");
+        Driver.closeDriver();
+    }
+}
