dc006e7792bedc6f5d27f72cd0b1fa274412e036
Tall Boy <tallboy6077@gmail.com>
2023-02-16 21:02:10 +0300

    ListenerTest4

---
+++ src/test/java/tests/listeners/Day27_ListenersTest4.java
@@ -0,0 +1,46 @@
+package tests.listeners;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+import utilities.Driver;
+public class Day27_ListenersTest4 {
+    @Test
+    public void test1(){
+        System.out.println("PASS");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test2(){
+        System.out.println("FAIL");
+//        throw new NoSuchElementException("No Such Element Exception");
+        Driver.getDriver().get("https://www.amazon.com");
+        Driver.getDriver().findElement(By.xpath("asdgadfhadfgnh"));//FAIL ETSIN
+    }
+    @Test
+    public void test3(){
+        System.out.println("SKIP");
+        throw new SkipException("Metotu Atla");
+    }
+    @Test
+    public void test4(){
+        System.out.println("EXCEPTION");
+//        throw new NoSuchElementException("No Such Element Exception");
+        Driver.getDriver().get("https://www.techproeducation.com");
+        Driver.getDriver().findElement(By.xpath("asdgadfhadfgnh"));//FAIL ETSIN
+    }
+}
+/*
+1. Failed test caseleri tekrar run etme islemini testNG xml file ile tum test caselere automatic olarak uygulayabiliriz.
+Util class olustur : ListenersRetryAnalyzer
+Implement IAnnotationTransformer interface
+transform metotunu olustur
+Metot da setRetryAnalyzer kullan: annotation.setRetryAnalyzer(ListenersRetry.class)
+2. xml file da bu util classi test caselerle ilişkilerdir
+<listeners>
+    <listener class-name=“techproed.utilities.ListenersRetryAnalyzer"></listener>
+</listeners>
+3. When I run this class
+
+ */
