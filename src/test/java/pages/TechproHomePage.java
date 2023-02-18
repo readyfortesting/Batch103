976dcee31bed02856f198b02e8ce26b36756f7b9
Tall Boy <tallboy6077@gmail.com>
2023-02-18 21:32:57 +0300

    TechProHomePage

---
+++ src/test/java/pages/TechproHomePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class TechproHomePage {
+    public TechproHomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//div[@role='alert']")
+    public WebElement homeHeader;
+    @FindBy(xpath = "//*[contains(text(),'Logout')]")
+    public WebElement homeLogoutButton;
+}
