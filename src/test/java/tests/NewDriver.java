a3d1bcb09aff13a007817bd296f0fbec9d6d34d9
B.Akyuz <readyforwrk@gmail.com>
2023-03-02 20:48:05 +0300

    NewDriver

---
+++ src/test/java/tests/NewDriver.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+public class NewDriver {
+    @Test
+    public void testName() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        Driver.closeDriver();
+        //Driver.getDriver().get("https://techproeducation.com");
+        //Driver.getDriver().get("https://facebook.com");
+    }
+}
